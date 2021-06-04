8caeda8e655f0d9106f1805e58fbfa5a6e3adf8f
Watotacho <watotacho@gmail.com>
2021-06-05 00:28:15 +0300

    update

--- bank.py
+++ bank.py
@@ -1,12 +1,40 @@
-class Bank:
-    def __init__(self,name,accountNumber,owner,balance):
+class Account:
+    def __init__(self,name,phone,loan,loan_limit):
           self.name=name
-          self.accountNumber=accountNumber
-          self.owner=owner
-          self.balance=balance
-    def deposit(self):
-        return f"The rich man is {self.name} his accountNumber is {self.accountNumber}.is a{self.type}"
-    def Amount(self):
-        return 50,000+self.balance
-    def result(self):
-        return f"Hello sarah your balance is {self.balance}"
+          self.phone=phone
+          self.balance =0
+          self.transaction=500
+          self.loan=0
+          self.loan_fees=0.20
+          self.loan_limit=loan_limit
+    def deposit(self,amount):
+        if amount<=0:
+            self.balance += amount
+            return f" Hello {self.name}.you have deposited{amount}your new balance is{self.balance}"
+        else:
+           return "please deposit a valid amount"
+
+
+
+
+
+
+    def widthdraw(self,amount):
+        total=amount+self.transaction
+        if amount<=0:
+            return "valid amount"
+        elif total> self.balance:
+            return "insufficient balance"
+        else:
+            self.balance=total
+            return (f" Hello {self.name} you have withdrawn {amount}.your new balance is {self.balance}")
+
+
+    def borrow(self,amount):
+        if self.loan>=0:
+            return "your transaction is unsuccessful.pay an existing loan"
+        else:
+            amount<=self.loan_limit
+            self.balance<=amount
+            self.loan<=amount
+            return( f" your loan is{amount} and you new balance is{self.balance}")
